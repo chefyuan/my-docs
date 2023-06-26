8df67974bbde0b266ab4f27158c19f1039fe02c6
厨子 <tan45duyuan@163.com>
2023-06-26 23:20:20 +0800

    TCP 服务器生成

--- RustProject/sensor/src/main.rs
+++ RustProject/sensor/src/main.rs
@@ -1,6 +1,11 @@
+use std::thread;
+
 use actix_web::App;
 use actix_web::{post, web, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
+use actix_rt::System;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
 
 //获取某个机器的温湿度
 #[derive(Deserialize)]
@@ -72,19 +77,32 @@ async fn get_all_sensor_data_fun(post_data: web::Json<AllTemAndHumData>) -> Http
     HttpResponse::Ok().body("Hello world11111111!")
 }
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let http_server = HttpServer::new(|| {
         App::new()
             .service(web::scope("/api")
                 .route("/GetOneSensorData", web::post().to(get_one_sensor_data_fun))
                 .route("/GetAllSensorData", web::post().to(get_all_sensor_data_fun))
             )
-    })
-    .bind("127.0.0.1:8000")
-    .expect("fail to bindin")
-    .run()
-    .await
+    }).bind("127.0.0.1:8000")?;
+
+    // 启动 HTTP 服务器
+    let http_server_handle = http_server.run();
+    
+    // 创建 TCP 监听器并处理传入的连接
+    let tcp_listener = TcpListener::bind("127.0.0.1:9000")?;
+    for stream in tcp_listener.incoming() {
+        let stream = stream?;
+        thread::spawn(move || {
+            // 处理客户端连接的代码
+        });
+    }
+
+    // 等待 HTTP 服务器运行完毕并关闭
+    http_server_handle.await?;
+
+    Ok(())
 }
+
 
